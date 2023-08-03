de600338dbc0e4212da55e7228ef5c26b372527a
root <root@DESKTOP-QS0QAE8>
2023-08-03 17:01:14 +0100

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 '''Lockboxes solution'''
-
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# boxes = [[1], [2], [3], [4], []]
+# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 
 def canUnlockAll(boxes):
     '''appends numbers in individual arrays to a new keys array
-       without any being repeated and then compares the length of the
-       keys array to the boxes array.
+        without any being repeated and then compares the length of the
+        keys array to the boxes array.
     '''
-    key = []
-    for item in boxes:
-        if not item:
-            return False
-        else:
-            for val in item:
-                if val not in key:
-                    key.append(val)
-        if len(key) == len(boxes) - 1:
-            return True
+
+    opened_boxes = set()
+    opened_boxes.add(0)  # Start with the first box (index 0) as opened
+    keys_to_check = boxes[0]
+
+    while keys_to_check:
+        key = keys_to_check.pop()
+        if key < len(boxes) and key not in opened_boxes:
+            opened_boxes.add(key)
+            keys_to_check.extend(boxes[key])
+
+    return len(opened_boxes) == len(boxes)
