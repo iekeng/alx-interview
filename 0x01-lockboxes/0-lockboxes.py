442f5903ea70cabe60450d8926c39c97718951e9
root <root@DESKTOP-QS0QAE8>
2023-08-03 17:02:51 +0100

    recommenting

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 '''Lockboxes solution'''
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-# boxes = [[1], [2], [3], [4], []]
-# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+
 
 def canUnlockAll(boxes):
     '''appends numbers in individual arrays to a new keys array
