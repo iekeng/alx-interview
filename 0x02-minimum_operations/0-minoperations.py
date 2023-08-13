822a750098cfaf6ede2f6453654bf3e15602f2de
root <root@DESKTOP-QS0QAE8>
2023-08-13 23:41:13 +0100

    err fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,7 +11,7 @@ def minOperations(n: int) -> int:
     operations = 0
 
     while (n > 1):
-        if n % divisor== 0:
+        if n % divisor == 0:
             n //= divisor
             operations += divisor
         else:
