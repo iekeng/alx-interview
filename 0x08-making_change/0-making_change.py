5208ca04bc947a8405e8ee272ed144284847b4e6
root <root@DESKTOP-QS0QAE8>
2023-09-20 14:02:02 +0100

    making change app

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+def makeChange(coins, total):
+    coins.sort()
+    amount = 0
+
+    if total <= 0:
+        return 0
+
+    while total > 0 and coins:
+        highest = coins[-1]
+
+        if highest <= total:
+            num_coins = total // highest
+            amount += num_coins
+            total -= num_coins * highest
+        else:
+            coins.pop()
+            if coins[-1] > total:
+                return -1
+
+    return amount
