f357275e8c65c1f86d063873e2a08e17b81c4843
root <root@DESKTOP-QS0QAE8>
2023-09-24 05:48:58 +0100

    refactored

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,22 +9,17 @@ def makeChange(coins, total):
         -total
         Calculates the minimum amount of coins required to make change
     """
-    coins.sort()
-    amount = 0
+    coins = sorted(coins, reverse=True)
+    count = 0
 
     if total <= 0:
         return 0
 
-    while total > 0 and coins:
-        highest = coins[-1]
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
 
-        if highest <= total:
-            num_coins = total // highest
-            amount += num_coins
-            total -= num_coins * highest
-        else:
-            coins.pop()
-            if coins[-1] > total:
-                return -1
-
-    return amount
+    if total == 0:
+        return count
+    return -1
